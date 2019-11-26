65808448c330afa521b92cc011567c87cd5e87ce
sabee4 <sabeenap@gmail.com>
2019-11-26 06:17:42 +0000

    Signup Form with SignupComponent

--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,16 +1,18 @@
 import Layout from '../components/Layout'
 import Link from 'next/link'
+import SignupComponent from '../components/auth/SignupComponent'
 
 const Signup = () => {
     return (
+            
             <Layout>
-               
-            <h2> Signup Page </h2>
-            <Link href="/ ">
-            <a>Home--</a>
-            </Link>
-            <a href="/">Home </a>
+                <h2>
+                    Signup Page
+                </h2>
+                <SignupComponent />
             </Layout>
+           
+        
            
        
     );
